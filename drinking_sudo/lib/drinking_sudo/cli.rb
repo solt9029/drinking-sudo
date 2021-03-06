a7b5236a4a699072d9aa876023b562da58d92748
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-03-06 09:43:11 +0900

    Update

--- drinking_sudo/lib/drinking_sudo/cli.rb
+++ drinking_sudo/lib/drinking_sudo/cli.rb
@@ -1,12 +1,22 @@
 require "drinking_sudo"
 require "thor"
 require "launchy"
+require "serialport"
 
 module DrinkingSudo
   class CLI < Thor
-    desc "camelize {snake_case_string}", "convert {snake_case_string} to {camelCaseString}"
-    def camelize(str)
-      puts str.split("_").map{|w| w[0] = w[0].upcase; w}.join
+    desc "serial", "serial"
+    def serial
+      serial_port = SerialPort.new("/dev/cu.usbserial-14240", 9600)
+      interval_second = 0.5
+      duration = 5
+      count = 0
+      while count < duration / interval_second do
+        sleep(interval_second)
+        sensor_value = serial_port.readline.chomp.strip.to_i
+        puts(sensor_value)
+        count += 1
+      end
     end
 
     desc "launch_website", "launch website"
@@ -15,15 +25,6 @@ module DrinkingSudo
         Launchy.open(url)
       end
       system("/usr/bin/sudo ls")
-    end
-
-    desc "snake {CamelCaseString}", "convert {CamelCaseString} to {snake_case_string}"
-    def snake(str)
-      puts str
-        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-        .tr("-", "_")
-        .downcase
     end
   end
 end
